c6f07a9ba6e758bc43aa49fe441e0bac0db1a470
blandflakes <xclite@gmail.com>
2016-03-19 18:33:49 -0400

    Functioning panning and zooming.

--- components/visualizer.js
+++ components/visualizer.js
@@ -28,6 +28,16 @@ var Axis = React.createClass({
  * TODO: Why hardcoded 25 and 30 for axis offsets
  */
 var GenomeGraph = React.createClass({
+  addZoom: function() {
+    var node = this.refs.svg;
+    d3.select(node).call(this.props.zoom);
+  },
+  componentDidMount: function() {
+    this.addZoom();
+  },
+  componentDidUpdate: function() {
+    this.addZoom();
+  },
   render: function() {
     var xScale = this.props.scales.x;
     var yScale = this.props.scales.y;
@@ -42,7 +52,8 @@ var GenomeGraph = React.createClass({
     return (
       <div className="genomeGraph">
         <svg width={this.props.width + this.props.margin.left + this.props.margin.right}
-             height={this.props.height + this.props.margin.top + this.props.margin.bottom}>
+             height={this.props.height + this.props.margin.top + this.props.margin.bottom}
+             ref="svg" >
           <g transform={graphTranslate}>
             <Axis translate={"translate(0," + this.props.height + ")"} orient="bottom" scale={xScale} />
             <text x={this.props.width / 2} y={this.props.height + 30} style={{textAnchor: "middle"}}>Position in genome</text>
@@ -76,17 +87,65 @@ export var GenomeVisualizer = React.createClass({
   height: 250,
   margin: { top: 10, right: 20, bottom: 50, left: 50 },
   readCeiling: 1000,
+  genomeLength: 2821361, // TODO get this from current genome rather than hardcoding
   getInitialState: function() {
     var xScale = d3.scale.linear().domain([0, this.width]).range([0, this.width]);
     var yScale = d3.scale.linear().domain([0, this.readCeiling]).range([this.height, 0]);
+    var self = this;
+    var zoom = d3.behavior.zoom().x(xScale).scaleExtent([1, 10]).on("zoom", function() {
+      if (xScale.domain()[0] < 0) {
+        window.console.log("Detected panning visualization too far to the right. Resetting x domain to [0, width].");
+        zoom.translate([zoom.translate()[0] - xScale(0) + xScale.range()[0], zoom.translate()[1]]);
+        window.console.log("x domain: " + xScale.domain());
+      }
+      else if (xScale.domain()[1] > this.genomeLength) {
+        window.console.log("Detected panning visualization too far to the left. Resetting x domain to [genomeLength, genomeLength - width]");
+        zoom.translate([zoom.translate()[0] - xScale(this.genomeLength) + xScale.range()[1], zoom.translate()[1]]);
+        window.console.log("x domain: " + xScale.domain());
+      }
+      // We never translate the y domain, but we will scale the height.
+      yScale.domain([0, self.readCeiling / zoom.scale()]);
+      self.forceUpdate();
+    });
     return { selectedInsertionMapName: null, positionalInput: "",
-             scales: { x: xScale, y: yScale } };
+             scales: { x: xScale, y: yScale }, zoom: zoom };
   },
   updatePositionalInput: function(e) {
     this.setState({ positionalInput: e.target.value });
   },
   panToPosition: function() {
-    window.console.log("Should pan to: " + this.state.positionalInput);
+    var target = this.state.positionalInput;
+    var targetIndex;
+    if (isNaN(target)) {
+      // Hopefully a string label.
+      var map = this.props.genomes["saouhsc"].map; // TODO this is hardcoded and sad
+      var label = map[target];
+      if (exists(label)) {
+        targetIndex = (label.start + label.end) / 2;
+      }
+      else {
+        alert("Couldn't find a genome named '" + target + "'");
+        return;
+      }
+    }
+    else {
+      targetIndex = parseInt(target);
+      if (targetIndex > this.genomeLength) {
+        alert("Target index is greater than the size of the genome!");
+        return;
+      }
+      if (targetIndex < 0) {
+        alert("Can't pan to a negative index!");
+        return;
+      }
+    }
+    this.state.zoom.translate([this.state.zoom.scale() * (0 - targetIndex), 0]);
+    this.forceUpdate();
+    /*
+      d3.transition()
+        .duration(50)
+        .call(zoom.translate([zoom.scale() * (0 - target_index), 0]).event);
+    TODO remove this if we don't need it */
   },
   updateSelectedInsertionMap: function(e) {
     this.setState({ selectedInsertionMapName: e.target.value });
@@ -108,7 +167,7 @@ export var GenomeVisualizer = React.createClass({
             <button onclick={this.props.removeDisplayedMap}>Remove this graph</button>
           </div>
           <GenomeGraph scales={this.state.scales} width={this.width} height={this.height} margin={this.margin}
-            visibleMappings={visibleMappings} />
+            visibleMappings={visibleMappings} zoom={this.state.zoom} />
         </div>
       );
     }.bind(this));
